a3cf4f43b87f70c23f6568eff50be61b65e90e1b
pedroportacio <56261114+pedroportacio@users.noreply.github.com>
2019-11-06 19:15:57 -0300

    Initial commit

---
+++ src/components/Games.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Games() {
+  return (
+    <div>
+      <h1>Jogos Mais Populares</h1>
+    </div>
+  );
+}
+export default Games;
---
+++ src/components/Streams.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Stream() {
+  return (
+    <div>
+      <h1>Streams mais populares</h1>
+    </div>
+  );
+}
+
+export default Stream;
